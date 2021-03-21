7d12583b4a62a75c71af0bccc21c7a747a91a965
phamthao <phamthao03596@gmail.com>
2021-03-21 23:08:14 +0700

    feat: add query log

---
+++ app/Providers/QueryLogServiceProvider.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Database\Events\QueryExecuted;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\ServiceProvider;
+
+class QueryLogServiceProvider extends ServiceProvider
+{
+
+    public function register()
+    {
+    }
+
+    public function boot()
+    {
+        if (!$this->app['config']->get('logging.query.enabled', false)) {
+            return;
+        }
+
+        $trigger = $this->app['config']->get('logging.query.trigger');
+
+        if (!empty($trigger) && !$this->requestHasTrigger($trigger)) {
+            return;
+        }
+
+        DB::listen(function (QueryExecuted $query) {
+            if ($query->time < $this->app['config']->get('logging.query.slower_than', 0)) {
+                return;
+            }
+
+            $sqlWithPlaceholders = str_replace(['%', '?'], ['%%', '%s'], $query->sql);
+
+            $bindings = $query->connection->prepareBindings($query->bindings);
+            $pdo = $query->connection->getPdo();
+            $realSql = $sqlWithPlaceholders;
+            $duration = $this->formatDuration($query->time / 1000);
+
+            if (count($bindings) > 0) {
+                $realSql = vsprintf($sqlWithPlaceholders, array_map([$pdo, 'quote'], $bindings));
+            }
+            Log::debug(sprintf('[%s] [%s] %s | %s: %s', $query->connection->getDatabaseName(), $duration, $realSql,
+                request()->method(), request()->getRequestUri()));
+        });
+    }
+
+    public function requestHasTrigger(string $trigger): bool
+    {
+        return false !== getenv($trigger) || \request()->hasHeader($trigger) || \request()->has($trigger) || \request()->hasCookie($trigger);
+    }
+
+    private function formatDuration(float $seconds): string
+    {
+        if ($seconds < 0.001) {
+            return round($seconds * 1000000).'μs';
+        } elseif ($seconds < 1) {
+            return round($seconds * 1000, 2).'ms';
+        }
+
+        return round($seconds, 2).'s';
+    }
+
+}
