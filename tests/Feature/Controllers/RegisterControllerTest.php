e77c87f6349f914434f99a877637aa42ccd196f9
phamthao <phamthao03596@gmail.com>
2021-03-22 21:04:44 +0700

    chore: more assert on register

--- tests/Feature/Controllers/RegisterControllerTest.php
+++ tests/Feature/Controllers/RegisterControllerTest.php
@@ -7,6 +7,7 @@ use Illuminate\Auth\Events\Registered;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Support\Facades\Event;
+use Illuminate\Support\Facades\Hash;
 use Tests\TestCase;
 
 class RegisterControllerTest extends TestCase
@@ -14,6 +15,11 @@ class RegisterControllerTest extends TestCase
     /** @test */
     public function a_user_can_register()
     {
+        Hash::shouldReceive('make')
+            ->once()
+            ->with('password')
+            ->andReturn('hashed');
+
         Event::fake();
 
         $this->postJson(action(RegisterController::class), [
@@ -23,6 +29,12 @@ class RegisterControllerTest extends TestCase
                 'password_confirmation' => 'password',
             ])
             ->assertSuccessful();
+
+        $this->assertDatabaseHas('users', [
+            'name' => 'Test User',
+            'email' => 'test@test.app',
+            'password' => 'hashed',
+        ]);
 
         Event::assertDispatched(Registered::class);
     }
