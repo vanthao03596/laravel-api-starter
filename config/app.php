bd5e78884584a5c5f257bd712319dbdbc333393f
phamthao <phamthao03596@gmail.com>
2021-03-22 10:27:24 +0700

    chore: disable blade

--- config/app.php
+++ config/app.php
@@ -160,7 +160,7 @@ return [
         // Illuminate\Session\SessionServiceProvider::class,
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
-        Illuminate\View\ViewServiceProvider::class,
+//        Illuminate\View\ViewServiceProvider::class,
 
         /*
          * Package Service Providers...
@@ -226,7 +226,7 @@ return [
         'Str' => Illuminate\Support\Str::class,
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
-        'View' => Illuminate\Support\Facades\View::class,
+//        'View' => Illuminate\Support\Facades\View::class,
 
     ],
 
