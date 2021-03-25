b747659964e17f998259df4cf351dc60cdc689a3
phamthao <phamthao03596@gmail.com>
2021-03-25 21:45:33 +0700

    refactor: extract get token to method

--- app/Http/Controllers/API/RefreshTokenController.php
+++ app/Http/Controllers/API/RefreshTokenController.php
@@ -4,13 +4,15 @@ namespace App\Http\Controllers\API;
 
 use App\Models\RefreshToken;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Cookie;
 use Jiannei\Response\Laravel\Support\Facades\Response;
 
 class RefreshTokenController
 {
     public function __invoke(): JsonResponse
     {
-        $refreshToken = request('refresh_token');
+        $refreshToken = $this->tokenFromRequest();
 
         if (! $refreshToken) {
             Response::errorUnauthorized();
@@ -30,14 +32,23 @@ class RefreshTokenController
             Response::errorUnauthorized();
         }
 
-        $token = auth()->tokenById($tokenable->id);
+        $token = Auth::tokenById($tokenable->id);
 
         return Response::success(
             [
                 'access_token' => $token,
                 'token_type' => 'bearer',
-                'expires_in' => auth()->factory()->getTTL() * 60,
+                'expires_in' => Auth::factory()->getTTL() * 60,
             ]
         );
+    }
+
+    private function tokenFromRequest()
+    {
+        if (Cookie::has('refresh_token')) {
+            return Cookie::get('refresh_token');
+        }
+
+        return request('refresh_token');
     }
 }
